e778f43f3b0b6e99b71631db35fdbc54cf7c19b8
Hussein <tahmazovhussejn@gmail.com>
2025-10-06 12:01:01 +0300

    dokument 36 on õpinud

--- Ineksid.sql
+++ Ineksid.sql
@@ -1,7 +1,14 @@
 -- Ineksid serveris
+
 use AdventureWorksDW2019;
 
 select * from DimEmployee
+
+/*
+Indekseid kasutatakse päringute tegemisel, mis annavad meile kiiresti andmeid.
+Indekseid luuakse tabelites ja vaadetes.
+Indeks tabelis või vaates on samasugune raamatu indeksile.
+*/
 
 Select * from DimEmployee where BaseRate > 10 and BaseRate < 20;
 
@@ -12,3 +19,39 @@ SELECT * FROM DimEmployee
 WITH (INDEX(IX_DimEmployee_Salary)) -- forumist stackoverflow
 
 exec sp_helptext DimEmployee; -- see ei tööta :<
+
+-- DROP INDEX index_name ON table_name;
+DROP INDEX PK_DimEmployee_EmployeeKey ON DimEmployee;
+
+
+/*Klastreeritud indeks dikteerib säilitatud andmete järjestuse tabelis
+ja seda saab klastreeritud puhul olla tabeli peale ainult üks. 
+Kui soovime luua teise klastreeritud indeksi Name veeru alusel,
+siis andmed tuleb järjestada selle veeru
+järgi ja see mõjutab järjestust teistmoodi*/
+
+-- Klastreeritud ja mitte-klastreeritud indeksid
+create clustered index IX_DimEmployee_Name
+on DimEmployee(FirstName);
+
+create clustered index IX_DimEmployee_Gender_Salary
+on DimEmployee(Gender DESC, BaseRate ASC);
+
+/*Mitte-klastreeritud indeks:
+Mitte-klastreeritud indeks on analoog indeksile õpikus.
+Andmed on säilitatud ühte kohta ja indeks teise kohta.
+Indeksil on suunajad e pointerid andmete salvestuskohta.
+Mitte-klastreeritud indeksid  on säilitatud eraldi tegelikust andmetest ja selle tulemusel saab tabel omandada rohkem,
+kui ühte mitte-klastreeritud indeksit. Analoogi võiks tuua raamatuga,
+kus indeksid on peatükkide järgi ja lõpus on lõpus olevate terminitega teine indeks.
+*/
+
+Create NonClustered Index IX_DimEmployee_Name
+on DimEmployee(FirstName);
+
+/*
+Erinevused klastreeritud ja mitte-klastreeritud indeksi vahel:
+1. Ainult üks klastreeritud indeks tabeli peale, samas saab olla rohkem, kui mitte-klastreeritud indeks
+2. Klastreeritud indeks on kiirem, kui mitte-klastreeritud indeks kuna mitte-klastreeritud indeks peab tagasi viitama tabelile. Seda juhul, kui selekteeritud veerg ei ole olemas indeksis.
+3. Klastreeritud indeks määratleb ära tabeliridade salvestusjärjestuse ja ei nõua kettal lisaruumi. Samas mitte-klastreeritud on salvestatud tabelist eraldi ja on vaja lisaruumi.
+*/

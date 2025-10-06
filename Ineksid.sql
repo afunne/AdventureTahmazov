3f9503c72dd54ce8daa8d5b80ae6489582e4c269
Hussein <tahmazovhussejn@gmail.com>
2025-10-06 10:53:57 +0300

    lisanud uus

--- Ineksid.sql
+++ Ineksid.sql
@@ -4,3 +4,11 @@ use AdventureWorksDW2019;
 select * from DimEmployee
 
 Select * from DimEmployee where BaseRate > 10 and BaseRate < 20;
+
+create index IX_DimEmployee_Salary
+on DimEmployee (BaseRate ASC)
+
+SELECT * FROM DimEmployee
+WITH (INDEX(IX_DimEmployee_Salary)) -- forumist stackoverflow
+
+exec sp_helptext DimEmployee; -- see ei tööta :<

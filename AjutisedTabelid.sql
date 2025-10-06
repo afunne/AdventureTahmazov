850d1915959f17b5a48cdde4495b47bc1aa168b0
Hussein <tahmazovhussejn@gmail.com>
2025-10-06 10:35:31 +0300

    inglis keeles kohalike ja globaalsete ajutiste tabelite erinevused

--- AjutisedTabelid.sql
+++ AjutisedTabelid.sql
@@ -1,3 +1,4 @@
+-- teine failist: Ajutised tabelid
 use AdventureWorksDW2019;
 
 Create Table #PersonDetails(Id int, FirstName nvarchar(20))
@@ -26,4 +27,12 @@ end;
 
 spCreateLocalTempTable;
 
+create table ##EmployeeDetails(id int, FirstName nvarchar(20));
 
+/*
+Differences between local and global temporary tables:
+1. Local temporary tables have one # sign, but global ones have two.
+2. SQL Server adds random numbers to the name of a local temporary table, but not to a global one.
+3. Local tables are only visible in the session that created them, but global tables are visible in all sessions.
+4. Local temporary tables are automatically deleted when the session that created them is closed, but global temporary tables are only terminated after the last connection is closed.
+*\

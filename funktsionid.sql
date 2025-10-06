75e6d536664e518ee16365a61d662e205a6be9a1
Hussein <tahmazovhussejn@gmail.com>
2025-10-06 09:41:07 +0300

    lisanud uus funktsion

--- funktsionid.sql
+++ funktsionid.sql
@@ -9,4 +9,17 @@ as
 Return (Select EmployeeKey, FirstName, LastName, cast(BirthDate as Date) as DOB
 From DimEmployee);
 
-select * from fn_ILTVF_GetEmployees()
+select * from fn_ILTVF_GetEmployees();
+
+insert into DimEmployee(EmployeeKey, FirstName, BirthDate, Gender, DepartmentName)
+values (297, 'Sam', '1977-09-10', 'M', 'Quality Assurance');
+
+Create function fn_GetEmployeeNameById(@Id int)
+
+Returns nvarchar(20)
+as
+begin
+return (select FirstName from DimEmployee Where EmployeeKey = @Id)
+end;
+
+select * from fn_GetEmployeeNameById();

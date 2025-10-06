e66b3b465580d0b472b3c60c5ba62d406070c168
Hussein <tahmazovhussejn@gmail.com>
2025-10-06 10:36:43 +0300

    eesti keeles kohalike ja globaalsete ajutiste tabelite erinevused

--- AjutisedTabelid.sql
+++ AjutisedTabelid.sql
@@ -36,3 +36,11 @@ Differences between local and global temporary tables:
 3. Local tables are only visible in the session that created them, but global tables are visible in all sessions.
 4. Local temporary tables are automatically deleted when the session that created them is closed, but global temporary tables are only terminated after the last connection is closed.
 *\
+
+/*
+Erinevused lokaalse ja globaalse ajutise tabeli osas:
+1. Lokaalsed ajutised tabelid on ühe # märgiga, aga globaalsel on kaks tükki.
+2. SQL server lisab suvalisi numbreid lokaalse ajutise tabeli nimesse, aga globaalse puhul seda ei ole.
+3. Lokaalsed on nähtavad ainult selles sessioonis, mis on selle loonud, aga globaalsed on nähtavad kõikides sessioonides.
+4. Lokaalsed ajutised tabelid on automaatselt kustutatud, kui selle loonud sessioon on kinni pandud, aga globaalsed ajutised tabelid lõpetatakse alles peale viimase ühenduse lõpetamist.
+*\

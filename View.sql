28100e59f1699433b7cf0193726e4168d6e31fc1
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-10-20 00:28:15 +0300

    soovitud tulemus

--- View.sql
+++ View.sql
@@ -1,4 +1,13 @@
 USE AdventureWorksDW2019;
 
-select * from DimEmployee
-select * from DimDepartmentGroup
+-- checking stuff
+select * from DimEmployee;
+select * from DimDepartmentGroupw;
+
+-- View serveris
+-- Selleks, et saada soovitud tulemus, me peaksime ühendama kaks tabelit omavahel
+SELECT EmployeeKey, FirstName, BaseRate, Gender, DepartmentName
+FROM DimEmployee
+JOIN DimDepartmentGroup 
+ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey;
+
